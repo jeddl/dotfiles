bfe33f638dfdd6eccc3a3303c265d0fa359302dc
Ethan Li <lmetaballsj@gmail.com>
2024-03-15 21:21:22 -0400

    Update lazyvim

--- .config/lazyvim/lua/plugins/neotree.lua
+++ .config/lazyvim/lua/plugins/neotree.lua
@@ -4,6 +4,20 @@ return {
     filesystem = {
       hijack_netrw_behavior = "disabled",
     },
+    window = {
+      width = 60,
+      mappings = {
+        ["<space>"] = "none",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "copy path to clipboard",
+        },
+      },
+    },
   },
   -- Do not open neo tree when opening a directory
   init = function() end,
