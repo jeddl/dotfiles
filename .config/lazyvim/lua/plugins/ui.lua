e48416ed0fc73bbc22c834786a7cab0b11e2bc0e
Ethan Li <ethan.li@stash.com>
2024-08-18 19:54:31 -0400

    lazyvim: show macro notification

--- .config/lazyvim/lua/plugins/ui.lua
+++ .config/lazyvim/lua/plugins/ui.lua
@@ -9,7 +9,16 @@ return {
           -- section_separators = { left = "", right = "" },
           -- component_separators = { left = "|", right = "|" },
         },
-        -- sections = {},
+        sections = {
+          lualine_x = {
+            {
+              -- Show macro notification in lualine
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            },
+          },
+        },
       }
     end,
   },
